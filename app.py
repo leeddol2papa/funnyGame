ca3fb7f1ad547c7ff8695632f3e52573b277f0cb
leeddol2papa <waaniipapa@gmail.com>
2026-07-10 11:51:08 +0900

    Update title in Streamlit app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hello Streamlit-er 👋")
+st.title("Hello Streamlit-erdddd 👋")
 st.markdown(
     """ 
     This is a playground for you to try Streamlit and have fun. 
